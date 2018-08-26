d0f7b4b24fbb65a619fbf7686ee0a702bd203a17
silverball12 <silvercharasilverball@gmail.com>
2018-08-26 15:20:49 +0900

    Initial commit

---
+++ app/src/main/java/masterung/androidthai/in/th/sutfriend/MainFragment.java
@@ -0,0 +1,41 @@
+package masterung.androidthai.in.th.sutfriend;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        RegisterControll();
+    }
+
+    private void RegisterControll() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentFragmentMain, new RegisterFragment()).addToBackStack(null).commit();
+            }
+        });
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+
+    }
+
+}   // Main Class
